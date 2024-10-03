11a9a53692d75de68de4d0862151ea2a6577fa1d
__ARNAV__ <79275041+Arnav-png@users.noreply.github.com>
2024-10-04 01:07:12 +0530

    Fix redis error

--- index.js
+++ index.js
@@ -1,67 +1,69 @@
 const http = require('http');
 const express = require('express');
-const Docker = require('dockerode')
-const managementApi = express();
+const Docker = require('dockerode');
 const httpProxy = require('http-proxy');
 
-const proxy = httpProxy.createProxy({})
-
-const docker = new Docker({socketPath:'/var/run/docker.sock'});
+const managementApi = express();
+const proxy = httpProxy.createProxy({});
+const docker = new Docker({ socketPath: '/var/run/docker.sock' });
 managementApi.use(express.json());
 
-const db = new Map()
+const db = new Map();
 
-docker.getEvents((err,stream)=>{
-    if(err){
+docker.getEvents((err, stream) => {
+    console.log('got event')
+    if (err) {
         console.log(err);
-        return
+        return;
     }
 
-    stream.on('data',async(chunk)=>{
-        if(!chunk) return;
+    stream.on('data', async (chunk) => {
+        if (!chunk) return;
 
-        const event = JSON.parse(chunk.toString())
-        if(event.type === 'container' && event.action === 'start'){
+        const event = JSON.parse(chunk.toString());
+        console.log("🚀 ~ stream.on ~ event:", event)
+        if (event.Type === 'container' && event.Action === 'start') {
             const container = docker.getContainer(event.id);
-            const containerinfo = await container.inspect()
+            const containerInfo = await container.inspect();
 
-            const containerName = containerinfo.Name.substring(1)
-            const ipAdress = containerinfo.NetworkSettings.IPAddress
+            const containerName = containerInfo.Name.substring(1);
+            const ipAddress = containerInfo.NetworkSettings.IPAddress;
 
-            const exposedPort = Object.keys(containerinfo.Config.ExposedPorts);
-            let defaultPort
-            if(exposedPort?.length>0){
-                const [port,type] = exposedPort[0].split('/')
-                if(type == 'tcp'){
+            const exposedPort = Object.keys(containerInfo.Config.ExposedPorts);
+            let defaultPort;
+            if (exposedPort?.length > 0) {
+                const [port, type] = exposedPort[0].split('/');
+                if (type === 'tcp') {
                     defaultPort = port;
                 }
             }
-            console.log(`registering ${containerName.localhost} ---> http://${ipAdress}:${defaultPort}`)
-            db.set(containerName , {containerName , ipAdress , defaultPort})
+
+            console.log(`Registering ${containerName} ---> http://${ipAddress}:${defaultPort}`);
+            db.set(containerName, { containerName, ipAddress, defaultPort });
         }
-    })
-})
+    });
+});
 
-
-const revereProxyApp = express();
-revereProxyApp.use((req,res)=>{
-    const hostname = req.body;
+const reverseProxyApp = express();
+reverseProxyApp.use((req, res) => {
+    const hostname = req.headers.host;
     const subdomain = hostname.split('.')[0];
 
-    if(!db.has(subdomain)){
-        return res.status(404).end(404)
+    if (!db.has(subdomain)) {
+        return res.status(404).end('404 Not Found');
     }
-    const {ipAdress , defaultPort} = db.get(subdomain);
-    const target = `http://${ipAdress}:${defaultPort}`
 
-    console.log(`forwarding ${hostname} -> ${target}`);
+    const { ipAddress, defaultPort } = db.get(subdomain);
+    const target = `http://${ipAddress}:${defaultPort}`;
 
-    return proxy.web(req,res,{target,changeOrigin:true})
-})
-const revereProxy = http.createSevrer(revereProxyApp);
+    console.log(`Forwarding ${hostname} -> ${target}`);
+    return proxy.web(req, res, { target, changeOrigin: true });
+});
 
-managementApi.post('/containers' , async(req , res)=>{
-    const {image, tag = 'latest'} = req.body
+const reverseProxy = http.createServer(reverseProxyApp);
+
+managementApi.post('/containers', async (req, res) => {
+    const { image, tag = 'latest' } = req.body;
     const images = await docker.listImages();
 
     console.log(images , 'these are my images')
@@ -77,32 +79,31 @@ managementApi.post('/containers' , async(req , res)=>{
        
     }
 
-    if (!imageAlreadyExist){
-        console.log('image is pulling by docker ::::' , `${image}:${tag}`);
-        await docker.pull(`${image}:${tag}`)
+    if (!imageAlreadyExist) {
+        console.log('Pulling image from Docker:', `${image}:${tag}`);
+        await docker.pull(`${image}:${tag}`);
     }
 
     const container = await docker.createContainer({
         Image: `${image}:${tag}`,
         Tty: false,
-        HostConfig:{
-            autoRemove:true,
+        HostConfig: {
+            autoRemove: true,
         }
-    })
+    });
 
     await container.start();
 
     return res.json({
-        status:'success',
-        container:`${await container.inspect().Name }.localhost`
-    })
-})
+        status: 'success',
+        container: `${(await container.inspect()).Name}.localhost`
+    });
+});
 
-managementApi.listen(8080 , ()=>{
-    console.log('started at port 8080')
-})
+managementApi.listen(8080, () => {
+    console.log('Management API started at port 8080');
+});
 
-revereProxy.listen(80,()=>{
-    console.log('reverse proxy listenting');
-    
-})
+reverseProxy.listen(80, () => {
+    console.log('Reverse proxy listening on port 80');
+});
